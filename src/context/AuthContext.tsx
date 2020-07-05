a2db51896463518c3f9d645664d4f0e06b031532
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-07-04 21:05:31 -0300

    06 - New: Storage token;

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,9 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
 
 interface LoginCredentials {
   email: string;
@@ -6,23 +11,50 @@ interface LoginCredentials {
 }
 
 interface AuthContextData {
-  name: string;
-  login(loginCredentials: LoginCredentials): boolean;
+  user: object;
+  login(loginCredentials: LoginCredentials): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
-  const login = useCallback(({ email, password }): boolean => {
-    if (email === 'admin@example.com' && password === 'admin') {
-      return true;
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@LoginHook:token');
+    const user = localStorage.getItem('@LoginHook:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
     }
 
-    return false;
+    return {} as AuthState;
+  });
+
+  const login = useCallback(({ email, password }): void => {
+    let response = {} as AuthState;
+
+    if (email === 'admin@example.com' && password === 'admin') {
+      response = {
+        token: 'user-token',
+        user: {
+          id: 'user-id',
+          name: 'User Full Name',
+          email: 'admin@example.com',
+          avatarUrl:
+            'https://gravatar.com/avatar/f5938e040ad9f0b8f2748b439e13495a',
+        },
+      };
+    }
+
+    const { token, user } = response;
+
+    localStorage.setItem('@LoginHook:token', token);
+    localStorage.setItem('@LoginHook:user', JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'lucas', login }}>
+    <AuthContext.Provider value={{ user: data.user, login }}>
       {children}
     </AuthContext.Provider>
   );
